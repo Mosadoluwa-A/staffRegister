ed7ca4a8c9f54c8435dcd0f9861f64dc1d96f21e
Mosadoluwa-A <magbonyin@gmail.com>
2021-02-02 14:06:41 +0100

    Improved Security

--- dfbclockin-project/dfbclockin/settings.py
+++ dfbclockin-project/dfbclockin/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-# import environ
+# import dotenv
+import os
+import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,11 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# env = environ.Env()
-# environ.Env.read_env()
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
-# SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = "'(x$56j#eulbygag^f(s1)a@lvgtx)#v3@1vteqjt#+2a(5r=b4'"
+SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = "'(x$56j#eulbygag^f(s1)a@lvgtx)#v3@1vteqjt#+2a(5r=b4'"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
